e8f2768ce68ea936ac667b23bbdce44fe91abf10
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-17 13:38:57 +0200

    feat: add toast features

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @stylistic/max-len */
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 const signInFormData = z.object({
@@ -13,22 +14,40 @@ const signInFormData = z.object({
 type SignInFormData = z.infer<typeof signInFormData>
 
 export const SignIn = () => {
-  const { register, handleSubmit, formState: { isSubmitting, errors } } = useForm<SignInFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<SignInFormData>()
 
-  async function handleSignIn({ email }:SignInFormData) {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    console.log({ email })
+  async function handleSignIn({ email }: SignInFormData) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      console.log({ email })
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn({ email }),
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
     <>
       <Helmet title="login" />
       <div className="p-8">
-        <div className="w-[350px] flex flex-col justify-center gap-6">
-
+        <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Acessar Painel</h1>
-            <p className="text-sm text-muted-foreground ">Acompanhe suas vendas pelo painel do parceiro!</p>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
           </div>
 
           <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
@@ -37,11 +56,8 @@ export const SignIn = () => {
               <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button
-              disabled={isSubmitting}
-              className="w-full"
-              type="submit"
-            >Acessar painel
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar painel
             </Button>
           </form>
         </div>
