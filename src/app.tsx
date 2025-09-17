34a12b4f0bf0da9e2ebd32760f4d310cc0f7d75e
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-17 09:11:46 +0200

    config shadcn-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,12 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-2xl text-shadow-black text-zinc-900 underline">
+        Hello world!
+      </h1>
+      <Button variant="destructive">Click</Button>
+    </>
+  );
 }
