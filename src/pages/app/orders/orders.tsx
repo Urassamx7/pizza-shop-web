bc69e2629e5a53e7bc22589e771b1bdc55d6ab88
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-18 10:48:07 +0200

    feat: table components & filters

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,15 +1,12 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
-  TableBody,
-  TableCell,
-  TableHead,
+  TableBody, TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
+import { OrderTableRow } from './table/row'
+import { OrderTableFilters } from './table/filters'
 
 export function Orders() {
   return (
@@ -19,10 +16,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight ">Pedidos</h1>
       </div>
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <OrderTableFilters />
         <div className="border rounded-md">
           <Table>
             <TableHeader>
@@ -41,53 +35,7 @@ export function Orders() {
               {
                 Array.from({ length: 10 }).map((_, index) => {
                   return (
-                    <TableRow key={index}>
-                      <TableCell>
-                        <Button variant="outline" size="xs">
-                          <Search className="w-3 h-3" />
-                          <span className="sr-only">Detalhes do pedido</span>
-                        </Button>
-                      </TableCell>
-
-                      <TableCell
-                        className="font-mono text-xs font-medium"
-                      >abADjksandasdasd
-                      </TableCell>
-                      <TableCell
-                        className="text-muted-foreground"
-                      >há 15 minutos
-                      </TableCell>
-                      <TableCell>
-                        <div className="flex items-center gap-2">
-                          <span className="h-2 w-2 rounded-full bg-slate-400" />
-                          <span
-                            className="font-medium text-muted-foreground"
-                          >Pendente
-                          </span>
-                        </div>
-                      </TableCell>
-                      <TableCell className="font-medium">
-                        Marvin Mussacate
-                      </TableCell>
-                      <TableCell className="font-medium">
-                        R$ 59,90
-
-                      </TableCell>
-                      <TableCell>
-
-                        <Button variant="outline" size="xs">
-                          <ArrowRight className="mr-2 h-3 w-3" />
-                          Aprovar
-                        </Button>
-
-                      </TableCell>
-                      <TableCell>
-                        <Button variant="ghost" size="xs">
-                          <X className="mr-2 h-3 w-3" />
-                          Cancelar
-                        </Button>
-                      </TableCell>
-                    </TableRow>
+                    <OrderTableRow key={index} />
                   )
                 })
              }
---
+++ src/pages/app/orders/table/row.tsx
@@ -0,0 +1,57 @@
+import { Button } from '@/components/ui/button'
+import { TableRow, TableCell } from '@/components/ui/table'
+import { Search, ArrowRight, X } from 'lucide-react'
+
+// interface OrderTableRowProps {}
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="w-3 h-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+
+      <TableCell
+        className="font-mono text-xs font-medium"
+      >abADjksandasdasd
+      </TableCell>
+      <TableCell
+        className="text-muted-foreground"
+      >há 15 minutos
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span
+            className="font-medium text-muted-foreground"
+          >Pendente
+          </span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">
+        Marvin Mussacate
+      </TableCell>
+      <TableCell className="font-medium">
+        R$ 59,90
+
+      </TableCell>
+      <TableCell>
+
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
